0f158477e640d41a1f252d8876416f4328578bfd
aadspinner <ramchand.p@outlook.com>
2024-10-14 06:16:16 +0000

    Update 4 files
    - /eks-manifest1.1.yaml
    - /eks-node-grp1.1.yaml
    - /cluster-vars.sh
    - /.gitlab-ci.yml

---
+++ cluster-vars.sh
@@ -0,0 +1,16 @@
+
+# cluster-vars.sh
+export CLUSTER_NAME="dev-test5"
+export NODEGROUP_NAME="spotinstances"
+export NODE_COUNT="3"
+export NODE_MAX_SIZE="5"
+export REGION="ap-south-1"
+export AWSCLI_VERSION="2.11.2"
+export KUBECTL_VERSION="v1.3.0"
+export SSH_PUBLIC_KEY="k9"
+export VPC_ID="vpc-0db13f76ffd134754"
+export SECURITY_GROUP="sg-05577e05cf426744e"
+export SHARED_SECURITY_GROUP="sg-0221530a8428624d9"
+export SERVICE_CIDR="10.100.0.0/16"
+export VPC_CIDR="192.168.0.0/16"
+export AVAILABILITY_ZONES="ap-south-1a,ap-south-1b"
