822a5e8e726dcf644a6f895741a21224fabde802
aadspinner <ramchand.p@outlook.com>
2024-10-14 06:59:53 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /cluster-vars.sh

--- cluster-vars.sh
+++ cluster-vars.sh
@@ -1,6 +1,6 @@
 # cluster-vars.sh
-export CLUSTER_NAME="dev-test5"
-export NODEGROUP_NAME="spotinstances"
+export CLUSTER_NAME="dev-test1"
+export NODEGROUP_NAME="spotinstance1"
 export NODE_COUNT=3  # Remove quotes to ensure it's an integer
 export NODE_MAX_SIZE=5  # Remove quotes to ensure it's an integer
 export REGION="ap-south-1"
@@ -11,6 +11,6 @@ export VPC_ID="vpc-0db13f76ffd134754"
 export SECURITY_GROUP="sg-05577e05cf426744e"
 export SHARED_SECURITY_GROUP="sg-0221530a8428624d9"
 export SERVICE_CIDR="10.100.0.0/16"
-export VPC_CIDR="192.168.0.0/16"
+export VPC_CIDR="10.0.0.0/16"
 export AVAILABILITY_ZONES="ap-south-1a,ap-south-1b"
 
